b00495e0dbcfa2759bc0031fd2a846b9a0d8e681
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2022-07-22 14:44:02 -0300

    projeto

---
+++ src/geradoraDeFigurinhas.java
@@ -0,0 +1,22 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+
+public class geradoraDeFigurinhas {
+    
+    void cria() throws Exception{
+
+        //ler imagem
+        BufferedImage imagemOrigianal = ImageIO.read(new File("Imagens/TopMovies_1.jpg"));
+
+        //cria nova imagem em memómria com transparencia e com tamanha novo
+
+        //copiar a imagem original para novo imagem (em memoria)
+        
+        //escrever uma frase na nova imagem
+
+        //escrever a imagem em um arquivo
+    }
+}
---
+++ src/imdbApi.java
@@ -0,0 +1,41 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+//Iniciar comendo os passos do programa
+
+public class imdbApi {
+
+    public static void main(String[] args) throws Exception {
+        
+        //fazer a conexão HTTP e fazer a busca dos 250 filmes
+
+        //String url = "https://api.mocki.io/v2/549a5d8b"
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_q6h6s0ro";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient(); //criado usando o atalho "Ctrl + ."
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build(); //criação de um request da API - visitar javadoc para saber mais de http request
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        //System.out.println(body);
+        
+        //filtrar os dados que interessam (titulo, poster, classificação)
+        jsonParser parser = new jsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        /* System.out.println(listaDeFilmes.size());
+        System.out.println(listaDeFilmes.get(0)); */
+
+        //exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+    }
+}
