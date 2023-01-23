d8429bee42ffbe5e315cd54f44c6226c22a572f1
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2023-01-23 11:41:45 -0300

    .

--- src/geradoraDeFigurinhas.java
+++ src/geradoraDeFigurinhas.java
@@ -37,7 +37,7 @@ public class geradoraDeFigurinhas {
         grafics.setFont(font);
         
         //escrever uma frase na nova imagem
-        grafics.drawString("LOKURA HEIM", 200, novaAltura - 120);
+        grafics.drawString("Imagem", 200, novaAltura - 120);
 
         //escrever a imagem em um arquivo
         ImageIO.write(novaImagem, "png", new File(nomeArquivo));
